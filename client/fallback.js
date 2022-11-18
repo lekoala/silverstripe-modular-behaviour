968a507aaebc8167701a22b76f4337d2e56122b5
Thomas <thomas@lekoala.be>
2022-11-18 16:17:08 +0100

    wrap call

--- client/fallback.js
+++ client/fallback.js
@@ -1,11 +1,13 @@
 if ("noModule" in HTMLScriptElement.prototype) {
     // It's supported => don't replace
 } else {
-    var list = document.querySelectorAll("modular-behaviour");
-    for (var i = 0; i < list.length; i++) {
-        var el = list[i];
-        el.innerHTML = "This widget is not supported by your browser";
-        el.style.cssText =
-            "border:1px solid red;padding:1em;color:red;display:block;text-align:center;";
-    }
+    window.addEventListener("DOMContentLoaded", function () {
+        var list = document.querySelectorAll("modular-behaviour");
+        for (var i = 0; i < list.length; i++) {
+            var el = list[i];
+            el.innerHTML = "This widget is not supported by your browser";
+            el.style.cssText =
+                "border:1px solid red;padding:1em;color:red;display:block;text-align:center;";
+        }
+    });
 }
